bbae63a407c99fc9ce08bc9916ff4f0a8b3f7f0c
Manzom <matteomanzolillo@gmail.com>
2024-08-30 23:28:00 +0200

    Crafting fixes
    Changed crafting logic, still no code o7

--- Assets/Scripts/CalderonBehavior.cs
+++ Assets/Scripts/CalderonBehavior.cs
@@ -1,9 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CalderonBehavior : MonoBehaviour
 {
+    public GameObject poison; //item output
+    public List<GameObject> requests; //what it asks for the potion
+    public List<GameObject> ingredients; //randomizable needed ingredients
 
     void Update()
     {
