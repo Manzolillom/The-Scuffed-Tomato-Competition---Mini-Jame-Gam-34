2ed35c749d35b68e52ea28bafafd5622d32369e0
Manzom <matteomanzolillo@gmail.com>
2024-09-01 02:20:50 +0200

    basic camera behavior script
    added more camera options for the camera crafting minigame

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,10 +1,30 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public void MovePlayerCamera(Vector3 position, Quaternion rotation)
+    public List<Transform> cameraPositions = new List<Transform>();
+    int currentCamera = 404;
+
+    private void Start()
     {
+        //ChangeCameraPosition("ThatCamera") //The first camera it should look for
+    }
+
+    public void ChangeCameraPosition(string cameraPosition)
+    {
+        for(int i = 0; i < cameraPositions.Count; i++) 
+        {
+            if (cameraPositions[i].name == cameraPosition) 
+            {
+                currentCamera = i;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (currentCamera == 404) return;
+        transform.position = cameraPositions[currentCamera].position;
     }
 }
